30334d7c778ce33add9cf912b516841885d73e12
student <abdullahihamdiabass@gmail.com>
2021-06-25 07:42:06 +0300

    master

--- app/src/main/java/com/example/numbersapp/NumberRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/numbersapp/NumberRecyclerViewAdapter.kt
@@ -7,10 +7,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.math.BigInteger
 
-class NumberRecyclerViewAdapter(var numberList: List<BigInteger>):RecyclerView.Adapter<NumberRecyclerViewAdapter.NumberViewHolder>() {
+class NumberRecyclerViewAdapter(var numberList: List<BigInteger>):RecyclerView.Adapter<NumberViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-        var itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.numbers_list_item, parent, false)
+        var itemView = LayoutInflater.from(parent.context).inflate(R.layout.numbers_list_item, parent, false)
         return NumberViewHolder(itemView)
     }
 
@@ -21,11 +20,12 @@ class NumberRecyclerViewAdapter(var numberList: List<BigInteger>):RecyclerView.A
     override fun getItemCount(): Int {
         return numberList.size
     }
+}
 
     class NumberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var tvnumber = itemView.findViewById<TextView>(R.id.tvnumbers)
     }
-}
+
 
 
 
